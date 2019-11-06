41dae994d2492514fba88e397f11ee39ebbb2f50
ASTOUND\a.oleksiuk <a.oleksiuk@astoundcommerce.com>
2019-11-06 16:12:39 +0200

    Updates

---
+++ precache-manifest.e560a6dc0792add5e6f5a07f3bb52776.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e235b32b345cc2bf9f32b64be4db029f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e236057f29170cb07eeb",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "8a6c753f771764eab6bf",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e236057f29170cb07eeb",
+    "url": "/static/js/2.171b487c.chunk.js"
+  },
+  {
+    "revision": "8a6c753f771764eab6bf",
+    "url": "/static/js/main.fce5ea1e.chunk.js"
+  },
+  {
+    "revision": "5842a0e37fc11335c6f6",
+    "url": "/static/js/runtime-main.6a0e396a.js"
+  }
+]);
